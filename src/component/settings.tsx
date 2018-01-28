9de3c540de1470afe3fdaf0fe2333ddeb3aeea22
her0e1c1 <her0e1c1@gmail.com>
2018-01-28 17:43:03 +0800

    clear cache command

--- src/component/settings.tsx
+++ src/component/settings.tsx
@@ -1,5 +1,17 @@
 import * as React from 'react';
 import * as RN from 'react-native';
 
-const Settings = () => <RN.View style={{ flex: 1, backgroundColor: '#123' }} />;
+const Settings = () => (
+  <RN.View style={{ flex: 1, backgroundColor: '#123' }}>
+    <RN.Button
+      title="CLEAR CACHE"
+      onPress={() => {
+        RN.Alert.alert('Are you sure?', '', [
+          { text: 'OK', onPress: () => RN.AsyncStorage.clear() }, // TODO: update component
+          { text: 'Cancel', onPress: () => {} },
+        ]);
+      }}
+    />
+  </RN.View>
+);
 export default Settings;
