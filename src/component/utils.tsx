f098c100273ddd78b65583de23381bd7334959f4
her0e1c1 <her0e1c1@gmail.com>
2018-01-31 22:13:26 +0800

    Update style for LoadingIcon

--- src/component/utils.tsx
+++ src/component/utils.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import * as RN from 'react-native';
 
-const LoadingIcon = () => (
-  <RN.Modal transparent>
-    <RN.View
-      style={{
-        flex: 1,
-        backgroundColor: 'rgba(#333a)',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <RN.ActivityIndicator size="large" animating={true} />
-    </RN.View>
-  </RN.Modal>
+export const LoadingIcon = () => (
+  <RN.View
+    style={{
+      flex: 1,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      zIndex: 1,
+      backgroundColor: 'rgba(#333a)',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <RN.ActivityIndicator size="large" animating={true} />
+  </RN.View>
 );
