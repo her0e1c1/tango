2046378a29394fd9cd7257717d619bc50aca4a47
her0e1c1 <her0e1c1@gmail.com>
2018-02-16 21:50:56 +0800

    Use navigate then go to deck swiper page
    todo: refactor

--- src/component/cardList.tsx
+++ src/component/cardList.tsx
@@ -6,10 +6,15 @@ import * as Action from 'src/action';
 import * as I from 'src/interface';
 import * as SD from './styled';
 import MasteredCircle from './masteredCircle';
+import { withNavigation } from 'react-navigation';
+import * as Selector from 'src/selector';
 
+@withNavigation
 export class CardList extends React.Component<Props, {}> {
   render() {
-    if (this.props.cards.length <= 0) {
+    const { deck_id } = this.props.navigation.state.params;
+    const cards = Selector.getCardList(this.props.state, deck_id);
+    if (cards.length <= 0) {
       return (
         <RN.View
           style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
@@ -20,7 +25,7 @@ export class CardList extends React.Component<Props, {}> {
     }
     return (
       <RN.FlatList
-        data={this.props.cards.map((item, index) => ({
+        data={cards.filter(x => !!x).map((item, index) => ({
           ...item,
           key: item.id,
         }))}
@@ -43,7 +48,24 @@ export class CardList extends React.Component<Props, {}> {
                   flex: 1,
                   paddingVertical: 10,
                 }}
-                onPress={() => this.props.goTo({ card: item, index })}
+                onPress={() => {
+                  this.props.navigation.navigate('card', {
+                    card_id: item.id,
+                    deck_id: item.deck_id,
+                  });
+                  const cards = Action.getCardList(
+                    this.props.state,
+                    item.deck_id
+                  );
+                  let cardIndex = 0;
+                  for (let i = 0; i < cards.length; i++) {
+                    if (cards[i].id == item.id) {
+                      cardIndex = i;
+                    }
+                  }
+                  this.props.updateConfig({ cardIndex });
+                }}
+                // onPress={() => this.props.goTo({ card: item, index })}
                 onLongPress={() => alert(JSON.stringify(item))}
               >
                 <SD.CardTitle>{item.name}</SD.CardTitle>
@@ -56,15 +78,12 @@ export class CardList extends React.Component<Props, {}> {
   }
 }
 
-const mapStateToProps = (state: RootState) => ({
-  state,
-  cards: Action.getCurrentCardList(state),
-});
+const mapStateToProps = (state: RootState) => ({ state });
 const _mapStateToProps = I.returntypeof(mapStateToProps);
 const mapDispatchToProps = {
   deleteCard: Action.deleteCard,
-  goTo: Action.goTo,
   toggle: Action.toggleMastered,
+  updateConfig: Action.updateConfig,
 };
 type Props = typeof _mapStateToProps & typeof mapDispatchToProps;
 export default connect(mapStateToProps, mapDispatchToProps)(CardList);
