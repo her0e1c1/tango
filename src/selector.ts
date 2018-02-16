fb89006582eda07483cf1092c2061d5fc4eac300
her0e1c1 <her0e1c1@gmail.com>
2018-02-16 21:27:54 +0800

    Update Selector

--- src/selector.ts
+++ src/selector.ts
@@ -1,20 +1,30 @@
+export const getCurrentDeck = (state: RootState): Deck => {
+  const r = state.nav.routes[state.nav.index];
+  const deck_id = r && r.params && r.params.deck_id;
+  return state.deck[deck_id] || {};
+};
+
 export const getCardList = (state: RootState, deck_id: number) => {
   const all = state.card.byDeckId[deck_id] || [];
   return all.map(id => state.card.byId[id]);
 };
 
-export const getCurrentCard = (state: RootState) => {
-  const cards = getCurrentCardList(state);
-  if (state.nav.index) {
-    return cards[state.nav.index];
+export const getCurrentCard = (state: RootState): Card => {
+  const r = state.nav.routes[state.nav.index];
+  const deck_id = r && r.params && r.params.deck_id;
+  const cards = getCurrentCardList(state, deck_id);
+  if (state.config.cardIndex >= 0) {
+    return cards[state.config.cardIndex];
   }
-  return state.nav.card;
+  return {} as Card;
 };
-export const getCurrentCardList = (state: RootState): Card[] => {
-  const deck = state.nav.deck;
+export const getCurrentCardList = (
+  state: RootState,
+  deck_id: number
+): Card[] => {
   const config = state.config;
-  if (deck) {
-    const ids = state.card.byDeckId[deck.id] || [];
+  if (deck_id) {
+    const ids = state.card.byDeckId[deck_id] || [];
     const cards = ids
       .map(id => state.card.byId[id])
       .filter(c => !!c) // defensive
