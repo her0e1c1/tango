d2f09a0e652bb482dbffd90b4af3badb413e7799
her0e1c1 <her0e1c1@gmail.com>
2018-02-16 21:48:55 +0800

    Fix showing back button condition

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -4,22 +4,26 @@ import * as RN from 'react-native';
 import { connect } from 'react-redux';
 import * as Action from 'src/action';
 import * as I from 'src/interface';
-import SearchBar from './searchBar';
 import * as SD from './styled';
+import { withTheme } from 'styled-components';
+import { withNavigation } from 'react-navigation';
 
 const MainText = styled(RN.Text)`
   color: ${({ theme }: AppContext) => theme.titleColor};
   font-size: 18px;
 `;
 
+@withTheme
+@withNavigation
 export class Header extends React.Component<Props, {}> {
   render() {
     const { state } = this.props;
     if (!state.config.showHeader) {
       return <RN.View />;
     }
-    const { deck } = this.props.state.nav;
+    const showBackButton = this.props.state.nav.routes.length > 1;
     const card = Action.getCurrentCard(state);
+    const deck = Action.getCurrentDeck(state);
     return (
       <RN.View style={{ marginBottom: 10 }}>
         <RN.View
@@ -30,22 +34,21 @@ export class Header extends React.Component<Props, {}> {
           }}
         >
           <RN.View style={{ flexDirection: 'row' }}>
-            <MainText>TANGO {deck && `[${deck.name}]`}</MainText>
+            <MainText>TANGO {deck && deck.name && `[${deck.name}]`}</MainText>
             {card &&
               card.category && (
                 <SD.CardCategory>{card.category}</SD.CardCategory>
               )}
           </RN.View>
-          {deck && (
+          {showBackButton && (
             <RN.TouchableOpacity
-              onPress={() => this.props.goBack()}
+              onPress={() => this.props.navigation.goBack()}
               onLongPress={() => this.props.goHome()}
             >
               <MainText>{'< BACK'}</MainText>
             </RN.TouchableOpacity>
           )}
         </RN.View>
-        {!deck && <SearchBar />}
       </RN.View>
     );
   }
@@ -53,9 +56,6 @@ export class Header extends React.Component<Props, {}> {
 
 const mapStateToProps = (state: RootState) => ({ state });
 const _mapStateToProps = I.returntypeof(mapStateToProps);
-const mapDispatchToProps = {
-  goBack: Action.goBack,
-  goHome: Action.goHome,
-};
+const mapDispatchToProps = { goHome: Action.goHome };
 type Props = typeof _mapStateToProps & typeof mapDispatchToProps;
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
