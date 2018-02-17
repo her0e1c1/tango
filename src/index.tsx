d388f0305dcabd54102a10968cba719b76d68de6
her0e1c1 <her0e1c1@gmail.com>
2018-02-17 14:14:22 +0800

    connect ThemeProvider with redux state

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /// <reference path="./index.d.ts" />
 
 import * as React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { ThemeProvider } from 'styled-components';
@@ -10,6 +10,12 @@ import { LoadingIcon } from 'src/component/utils';
 import RootTabs from './component';
 import store from './store';
 import * as Action from 'src/action';
+
+const Theme = connect(state => ({ state }))(({ state }) => (
+  <ThemeProvider theme={Action.getTheme(state as RootState)}>
+    <RootTabs />
+  </ThemeProvider>
+));
 
 class Main extends React.Component {
   componentDidMount() {
@@ -20,9 +26,7 @@ class Main extends React.Component {
     return (
       <Provider store={store}>
         <PersistGate loading={<LoadingIcon />} persistor={persistStore(store)}>
-          <ThemeProvider theme={Action.getTheme(store.getState())}>
-            <RootTabs />
-          </ThemeProvider>
+          <Theme />
         </PersistGate>
       </Provider>
     );
