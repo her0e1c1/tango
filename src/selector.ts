b0a9059cbf56897333d4dc74ee264f6fd99a05e2
her0e1c1 <her0e1c1@gmail.com>
2018-02-17 22:00:02 +0800

    getMyDecks selector

--- src/selector.ts
+++ src/selector.ts
@@ -1,3 +1,12 @@
+export const getMyDecks = (state: RootState): Deck[] => {
+  const uid = state.user.uid;
+  if (state.share.user[uid]) {
+    return Object.values(state.share.user[uid].deck.byId);
+  } else {
+    return [];
+  }
+};
+
 export const getCurrentDeck = (state: RootState): Deck => {
   const r = state.nav.routes[state.nav.index];
   const deck_id = r && r.params && r.params.deck_id;
