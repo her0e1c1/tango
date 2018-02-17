8f29d7c83668d61b315e73e0f56a84f1bbcc8cb3
her0e1c1 <her0e1c1@gmail.com>
2018-02-17 13:57:10 +0800

    Fix Main component

--- src/index.tsx
+++ src/index.tsx
@@ -5,31 +5,27 @@ import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { ThemeProvider } from 'styled-components';
-import { connect } from 'react-redux';
 
+import { LoadingIcon } from 'src/component/utils';
 import RootTabs from './component';
 import store from './store';
 import * as Action from 'src/action';
 
-@connect((state: RootState) => ({ state }))
-class Wrap extends React.Component {
+class Main extends React.Component {
   componentDidMount() {
     store.dispatch(Action.auth.init());
     store.dispatch(Action.checkVersion(3));
   }
   render() {
     return (
-      <ThemeProvider theme={Action.getTheme(this.props.state)}>
-        <RootTabs />
-      </ThemeProvider>
+      <Provider store={store}>
+        <PersistGate loading={<LoadingIcon />} persistor={persistStore(store)}>
+          <ThemeProvider theme={Action.getTheme(store.getState())}>
+            <RootTabs />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
     );
   }
 }
-
-export default () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistStore(store)}>
-      <Wrap />
-    </PersistGate>
-  </Provider>
-);
+export default Main;
