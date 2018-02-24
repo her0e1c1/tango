55210e5cf517099337e5727659f128f9de5ca480
her0e1c1 <her0e1c1@gmail.com>
2018-02-24 23:23:26 +0800

    Add GOOGLE_AUTH_SCOPE

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,10 @@
 export * from 'src/secret';
 
 export const CURRENT_VERSION = 5;
+
+export const GOOGLE_AUTH_SCOPE = [
+  'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/drive.readonly',
+  'profile',
+  'email',
+];
