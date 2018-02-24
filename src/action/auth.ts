3f79dc3268238ef6d09f2124529842ab6934260c
her0e1c1 <her0e1c1@gmail.com>
2018-02-24 23:27:34 +0800

    Add refreshToken & loginWithGoogleOnWeb

--- src/action/auth.ts
+++ src/action/auth.ts
@@ -3,6 +3,84 @@ import * as I from 'src/interface';
 import * as C from 'src/constant';
 import * as firebase from 'firebase';
 import * as Action from 'src/action';
+import { AuthSession } from 'expo';
+import * as queryString from 'query-string';
+import { updateConfig } from 'src/action';
+
+export const refreshToken = (): I.ThunkAction => async (dispatch, getState) => {
+  const refresh_token = getState().config.googleRefreshToken;
+  if (!refresh_token) {
+    console.log(`You can't refresh`);
+    return false;
+  }
+  const body = queryString.stringify({
+    refresh_token,
+    grant_type: 'refresh_token',
+    client_id: C.GOOGLE_WEB_CLIENT_ID,
+    client_secret: C.GOOGLE_WEB_CLIENT_SECRET,
+  });
+  const json = await fetch('https://accounts.google.com/o/oauth2/token', {
+    method: 'POST',
+    body,
+    headers: new Headers({
+      'content-type': 'application/x-www-form-urlencoded',
+    }),
+  }).then(r => r.json());
+  await dispatch(updateConfig({ googleAccessToken: json.access_token }));
+};
+
+export const loginWithGoogleOnWeb = (): I.ThunkAction => async (
+  dispatch,
+  getState
+) => {
+  // need to register this url in admin console
+  const redirectUrl = AuthSession.getRedirectUrl();
+  const scope = C.GOOGLE_AUTH_SCOPE.join('%20');
+  const authUrl =
+    `https://accounts.google.com/o/oauth2/v2/auth?` +
+    `&client_id=${C.GOOGLE_WEB_CLIENT_ID}` +
+    `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+    `&response_type=code` +
+    `&access_type=offline` +
+    `&prompt=consent` +
+    `&scope=${scope}`;
+  try {
+    let result = await Expo.AuthSession.startAsync({ authUrl });
+    const body = queryString.stringify({
+      grant_type: 'authorization_code',
+      redirect_uri: redirectUrl,
+      client_id: C.GOOGLE_WEB_CLIENT_ID,
+      client_secret: C.GOOGLE_WEB_CLIENT_SECRET,
+      code: result.params.code,
+    });
+    const json = await fetch('https://accounts.google.com/o/oauth2/token', {
+      method: 'POST',
+      body,
+      headers: new Headers({
+        'content-type': 'application/x-www-form-urlencoded',
+      }),
+    }).then(r => r.json());
+    const credential = firebase.auth.GoogleAuthProvider.credential(
+      null, // you can skip id token
+      json.access_token
+    );
+    firebase
+      .auth()
+      .signInWithCredential(credential)
+      .then(async () => {
+        await dispatch(init());
+        await dispatch(
+          updateConfig({
+            googleAccessToken: json.access_token,
+            googleRefreshToken: json.refresh_token,
+          })
+        );
+      })
+      .catch(error => console.log(error));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const loginWithGoogle = (): I.ThunkAction => async (
   dispatch,
@@ -11,7 +89,8 @@ export const loginWithGoogle = (): I.ThunkAction => async (
   const result = await Expo.Google.logInAsync({
     androidClientId: C.GOOGLE_ANDROID_CLIENT_ID,
     iosClientId: C.GOOGLE_IOS_CLIENT_ID,
-    scopes: ['profile', 'email'],
+    // webClientId: C.GOOGLE_WEB_CLIENT_ID,
+    scopes: C.GOOGLE_AUTH_SCOPE,
   });
   const { type, idToken } = result;
   if (type === 'success') {
@@ -20,9 +99,7 @@ export const loginWithGoogle = (): I.ThunkAction => async (
       .auth()
       .signInWithCredential(credential)
       .then(() => dispatch(init()))
-      .catch(error => {
-        console.log(error);
-      });
+      .catch(error => console.log(error));
   } else {
     alert(`Can not login with Google account`);
   }
@@ -47,9 +124,7 @@ export const loginWithFacebook = (): I.ThunkAction => async (
         .auth()
         .signInWithCredential(credential)
         .then(() => dispatch(init()))
-        .catch(error => {
-          console.log(error);
-        });
+        .catch(error => console.log(error));
     }
   } catch (e) {
     alert(JSON.stringify(e));
