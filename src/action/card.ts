461ff19b0959a4501db677079fa9b97623212620
her0e1c1 <her0e1c1@gmail.com>
2018-02-25 18:53:03 +0800

    Refactor sqls for card

--- src/action/card.ts
+++ src/action/card.ts
@@ -1,62 +1,38 @@
 import * as I from 'src/interface';
-import { db } from 'src/store/sqlite';
+import { exec } from 'src/store/sqlite';
 
 export const updateCard = (card: Card): I.ThunkAction => async (
   dispatch,
   getState
 ) => {
-  db.transaction(tx =>
-    tx.executeSql(
-      `update card set name = ?, body = ?, hint = ? where id = ?;`,
-      [card.name, card.body, card.hint, card.id],
-      (_, result) => {
-        if (result.rowsAffected === 1) {
-          dispatch({ type: 'BULK_INSERT', payload: { cards: [card] } });
-        } else {
-          alert('You can not update');
-        }
-      },
-      (...args) => alert(JSON.stringify(args))
-    )
-  );
+  const sql = 'update card set name = ?, body = ?, hint = ? where id = ?;';
+  const result = await exec(sql, [card.name, card.body, card.hint, card.id]);
+  if (result.rowsAffected === 1) {
+    dispatch({ type: 'BULK_INSERT', payload: { cards: [card] } });
+  } else {
+    alert('You can not update');
+  }
 };
 
 export const deleteCard = (card: Card): I.ThunkAction => async (
   dispatch,
   getState
 ) => {
-  db.transaction(tx =>
-    tx.executeSql(
-      `delete from card where id = ?;`,
-      [card.id],
-      (_, result) => {
-        if (result.rowsAffected === 1) {
-          dispatch({ type: 'DELETE', payload: { card } });
-        } else {
-          alert('You can not delete');
-        }
-      },
-      (...args) => alert(JSON.stringify(args))
-    )
-  );
+  const result = await exec('delete from card where id = ?', [card.id]);
+  if (result.rowsAffected === 1) {
+    dispatch({ type: 'DELETE', payload: { card } });
+  } else {
+    alert('You can not delete');
+  }
 };
 
-// can config limit
 export const selectCard = (deck_id?: number): I.ThunkAction => async (
   dispatch,
   getState
 ) => {
-  db.transaction(tx =>
-    tx.executeSql(
-      `select * from card`,
-      [],
-      (_, result) => {
-        const cards = result.rows._array;
-        dispatch({ type: 'BULK_INSERT', payload: { cards } });
-      },
-      (...args) => alert(JSON.stringify(args))
-    )
-  );
+  const result = await exec('select * from card');
+  const cards = result.rows._array;
+  dispatch({ type: 'BULK_INSERT', payload: { cards } });
 };
 
 export const toggleMastered = (
@@ -64,52 +40,27 @@ export const toggleMastered = (
   mastered?: boolean
 ): I.ThunkAction => async (dispatch, getState) => {
   const m = mastered === undefined ? !card.mastered : mastered;
-  return new Promise((resolve, reject) =>
-    db.transaction(tx =>
-      tx.executeSql(
-        `update card set mastered = ? where id = ?`,
-        [m, card.id],
-        (_, result) => {
-          dispatch({
-            type: 'INSERT',
-            payload: { card: { ...card, mastered: m } },
-          });
-          resolve();
-        },
-        (...args) => reject(alert(JSON.stringify(args)))
-      )
-    )
-  );
+  const sql = 'update card set mastered = ? where id = ?';
+  await exec(sql, [m, card.id]);
+  dispatch({ type: 'INSERT', payload: { card: { ...card, mastered: m } } });
 };
 
 // FIXME: how can I bulk insert?
 export const bulkInsertCards = (
   deck_id: number,
   cards: Pick<Card, 'name' | 'body' | 'category' | 'hint' | 'fkid'>[]
-): I.ThunkAction => (dispatch, getState) =>
-  new Promise((resolve, reject) =>
-    db.transaction(async tx => {
-      await Promise.all(
-        cards.map(
-          card =>
-            new Promise(resolve =>
-              tx.executeSql(
-                `insert into card (name, body, category, hint, deck_id) values (?, ?, ?, ?, ?);`,
-                [card.name, card.body, card.category, card.hint, deck_id],
-                async (_, result) => {
-                  const id = result.insertId;
-                  id &&
-                    (await dispatch({
-                      type: 'INSERT',
-                      payload: { card: { ...card, deck_id, id } as Card },
-                    }));
-                  resolve();
-                },
-                (...args) => reject(alert(JSON.stringify(args)))
-              )
-            )
-        )
-      );
-      resolve();
-    })
-  );
+): I.ThunkAction => async (dispatch, getState) => {
+  const ps = cards.map(async card => {
+    const sql =
+      'insert into card (name, body, category, hint, deck_id) values (?, ?, ?, ?, ?);';
+    const values = [card.name, card.body, card.category, card.hint, deck_id];
+    const result = await exec(sql, values);
+    const id = result.insertId;
+    id &&
+      (await dispatch({
+        type: 'INSERT',
+        payload: { card: { ...card, deck_id, id } as Card },
+      }));
+  });
+  await Promise.all(ps);
+};
