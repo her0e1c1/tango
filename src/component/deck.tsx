5c2b1fa802152b2dff7641895ef57d461e957571
her0e1c1 <her0e1c1@gmail.com>
2018-02-28 21:51:19 +0800

    Move ProgressBar to deck.tsx

--- src/component/deck.tsx
+++ src/component/deck.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import * as RN from 'react-native';
+import * as SD from './styled';
 import { connect } from 'react-redux';
 import Swipeout from 'react-native-swipeout';
 import * as Action from 'src/action';
-import * as I from 'src/interface';
 import { DeckCard, DeckTitle } from './styled';
-import { ProgressBar } from './progressBar';
 import { withNavigation } from 'react-navigation';
 
 @withNavigation
@@ -87,6 +86,52 @@ export class DeckList extends React.Component<{}, { refreshing: boolean }> {
           );
         }}
       />
+    );
+  }
+}
+
+@connect(state => ({ state }))
+export class ProgressBar extends React.Component<
+  { state: RootState } & { deck_id: number; showCardIndex?: boolean },
+  {}
+> {
+  render() {
+    const deck_id = this.props.deck_id;
+    const index = this.props.showCardIndex
+      ? `(${this.props.state.config.cardIndex})`
+      : '';
+    const cards = Selector.getCardList(this.props.state, deck_id);
+    const mastered = cards.filter(x => !!x && x.mastered);
+    const width = cards.length > 0 ? mastered.length / cards.length * 100 : 0;
+    return (
+      <RN.View
+        style={{
+          height: 20,
+          backgroundColor: '#DEE2E6',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        <SD.ProgressBar width={`${width}%`} />
+        <RN.View
+          style={{
+            flex: 1,
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            alignItems: 'center',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            paddingRight: 5,
+          }}
+        >
+          <RN.Text
+            style={{
+              fontSize: 13,
+              fontWeight: 'bold',
+            }}
+          >{`${mastered.length}/${cards.length}${index}`}</RN.Text>
+        </RN.View>
+      </RN.View>
     );
   }
 }
