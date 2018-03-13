c76f5aa7c79636610e6667cac64992bca786b21e
her0e1c1 <her0e1c1@gmail.com>
2018-03-13 22:47:54 +0800

    up to 8

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
 export * from 'src/secret';
 
-export const CURRENT_VERSION = 7;
+export const CURRENT_VERSION = 8;
 
 export const GOOGLE_AUTH_SCOPE = [
   'https://www.googleapis.com/auth/spreadsheets',
