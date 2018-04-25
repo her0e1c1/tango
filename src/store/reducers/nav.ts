370c7fd0189302ee148eb28e3f5b215ca5968cee
her0e1c1 <her0e1c1@gmail.com>
2018-04-25 16:47:15 +0900

    Fix the way of initializing nav state

--- src/store/reducers/nav.ts
+++ src/store/reducers/nav.ts
@@ -1,10 +1,14 @@
 import * as Redux from 'redux';
+import { NavigationActions } from 'react-navigation';
 import Root from 'src/component';
 
 const initialState = Root.router.getStateForAction(
-  Root.router.getActionForPathAndParams('Main')
+  NavigationActions.init()
+  // WARN: some sample code recommends code like this
+  // Root.router.getActionForPathAndParams('Main')
+  // But in this case, it push a duplicated object to routes
+  // it may happen if you use nested routers
 );
-
 export default (
   state: NavState = initialState,
   action: Redux.Action
