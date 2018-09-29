02aee7639f63be0e48f2d89ee0c420423712eabc
her0e1c1 <her0e1c1@gmail.com>
2018-09-29 21:22:03 +0700

    rm console

--- src/selector.ts
+++ src/selector.ts
@@ -36,7 +36,6 @@ export const getCurrentDeck = (state: RootState): Deck => {
 
 export const getCardList = (state: RootState, deck_id: number) => {
   const all = state.card.byDeckId[deck_id] || [];
-  console.log('DEBUG: ', all.length, deck_id);
   return all.map(id => state.card.byId[id]);
 };
 
