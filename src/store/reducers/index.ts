084cdc877c8fb1f5745bf3b7bee3f1f6a41086fc
her0e1c1 <her0e1c1@gmail.com>
2018-09-29 21:47:09 +0700

    remove user state

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -2,6 +2,5 @@ import card from './card';
 import config from './config';
 import deck from './deck';
 import nav from './nav';
-import user from './user';
 
-export default { card, deck, config, nav, user };
+export default { card, deck, config, nav };
