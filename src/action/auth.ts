d45283d10891b55f10343e29904634475dbd3723
her0e1c1 <her0e1c1@gmail.com>
2018-09-29 21:51:28 +0700

    move code

--- src/action/auth.ts
+++ src/action/auth.ts
@@ -2,34 +2,6 @@ import * as C from 'src/constant';
 import * as Action from 'src/action';
 import * as queryString from 'query-string';
 
-export const refreshToken = (): ThunkAction<boolean> => async (
-  dispatch,
-  getState
-) => {
-  const refresh_token = getState().config.googleRefreshToken;
-  if (!refresh_token) {
-    console.log(`You can't refresh`);
-    return false;
-  }
-  const body = queryString.stringify({
-    refresh_token,
-    grant_type: 'refresh_token',
-    client_id: C.GOOGLE_WEB_CLIENT_ID,
-    client_secret: C.GOOGLE_WEB_CLIENT_SECRET,
-  });
-  const json = await fetch('https://accounts.google.com/o/oauth2/token', {
-    method: 'POST',
-    body,
-    headers: new Headers({
-      'content-type': 'application/x-www-form-urlencoded',
-    }),
-  }).then(r => r.json());
-  await dispatch(
-    Action.config.updateConfig({ googleAccessToken: json.access_token })
-  );
-  return true;
-};
-
 /*
 export const loginWithGoogleOnWeb = (): ThunkAction => async (
   dispatch,
--- src/action/index.ts
+++ src/action/index.ts
@@ -1,14 +1,41 @@
 import * as nav from './nav';
 import * as auth from './auth';
 import * as config from './config';
-import * as drive from './drive';
 
 export * from 'src/react-native/action';
 export * from 'src/web/action';
-export { auth, nav, config, drive };
+export { auth, nav, config };
 
 import * as Papa from 'papaparse';
 import * as WebAction from 'src/web/action';
+
+export const refreshToken = (): ThunkAction<boolean> => async (
+  dispatch,
+  getState
+) => {
+  const refresh_token = getState().config.googleRefreshToken;
+  if (!refresh_token) {
+    console.log(`You can't refresh`);
+    return false;
+  }
+  const body = queryString.stringify({
+    refresh_token,
+    grant_type: 'refresh_token',
+    client_id: C.GOOGLE_WEB_CLIENT_ID,
+    client_secret: C.GOOGLE_WEB_CLIENT_SECRET,
+  });
+  const json = await fetch('https://accounts.google.com/o/oauth2/token', {
+    method: 'POST',
+    body,
+    headers: new Headers({
+      'content-type': 'application/x-www-form-urlencoded',
+    }),
+  }).then(r => r.json());
+  await dispatch(
+    Action.config.updateConfig({ googleAccessToken: json.access_token })
+  );
+  return true;
+};
 
 export const insertByURL = (url: string): ThunkAction => async (
   dispatch,
@@ -61,3 +88,38 @@ export const insertByText = (text, deck): ThunkAction => (
   });
   */
 };
+
+/*
+export const importFromSpreadSheet = (
+  drive: Drive,
+  sheet: Sheet
+): ThunkAction => async (dispatch, getState) => {
+  const gid = sheet.properties.sheetId;
+  try {
+    await dispatch(Action.config.startLoading());
+    const res = await dispatch(
+      fetchAPI(
+        `https://docs.google.com/spreadsheets/d/${
+          drive.id
+        }/export?gid=${gid}&exportFormat=csv`
+      )
+    );
+    if (res.ok) {
+      const text = await res.text();
+      await dispatch(
+        Action.deck.insertByText(text, {
+          name: sheet.properties.title,
+          spreadsheetId: drive.id,
+          spreadsheetGid: String(gid),
+        })
+      );
+    } else {
+      alert('CAN NOT IMPORT');
+    }
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await dispatch(Action.config.endLoading());
+  }
+};
+*/
