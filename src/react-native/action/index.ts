ec11eb0d68ac1ad4416ff5a203b9ac8a15f759e9
her0e1c1 <her0e1c1@gmail.com>
2018-10-01 02:00:19 +0700

    move native code to react-native/action/index.ts

--- src/react-native/action/index.ts
+++ src/react-native/action/index.ts
@@ -1,8 +1,12 @@
 import * as Expo from 'expo';
 import * as C from 'src/constant';
 import * as firebase from 'firebase';
+import { NavigationActions } from 'react-navigation';
+
 import * as Action from 'src/action';
+import * as type from 'src/action/type';
 import { auth } from 'src/firebase';
+import * as Selector from 'src/selector';
 
 export * from 'src/action';
 
@@ -160,3 +164,132 @@ export const loginWithFacebook = (): ThunkAction => async (
   }
 };
 */
+
+export const goBack = () => async (dispatch, getState) => {
+  await dispatch(NavigationActions.back());
+};
+export const goTo = (routeName, params?) => async (dispatch, getState) => {
+  await dispatch(NavigationActions.navigate({ routeName, params }));
+};
+
+export const goToCardById = (card_id: number, deck_id: number) => async (
+  dispatch,
+  getState
+) => {
+  const a = NavigationActions.reset({
+    index: 1,
+    actions: [
+      NavigationActions.navigate({ routeName: 'home' }),
+      NavigationActions.navigate({
+        routeName: 'card',
+        params: { card_id, deck_id },
+      }),
+    ],
+  });
+  await dispatch(a);
+};
+
+export const shuffleCardsOrSort = (): ThunkAction => async (
+  dispatch,
+  getState
+) => {
+  const config = getState().config;
+  dispatch(type.card_shuffle(config));
+};
+
+export const goToNextCardSetMastered = (
+  mastered?: boolean
+): ThunkAction => async (dispatch, getState) => {
+  const state = getState();
+  const card = Selector.getCurrentCard(state);
+  if (card) {
+    await dispatch(Action.cardUpdate({ ...card, mastered: !card.mastered }));
+    if (state.config.showMastered) {
+      await dispatch(goToNextCard());
+    } else if (mastered === false) {
+      await dispatch(goToNextCard());
+    }
+  }
+};
+
+export const goToNextCardToggleMastered = () => goToNextCardSetMastered();
+export const goToNextCardNotMastered = () => goToNextCardSetMastered(false);
+export const goToNextCardMastered = () => goToNextCardSetMastered(true);
+
+export const goToNextCard = (): ThunkAction => async (dispatch, getState) => {
+  const state = getState();
+  const deck = Selector.getCurrentDeck(state);
+  const cards = Selector.getCurrentCardList(state);
+  const currentIndex = deck.currentIndex + 1;
+  if (currentIndex <= cards.length - 1) {
+    await dispatch(type.deckBulkInsert([{ ...deck, currentIndex }]));
+  } else {
+    await dispatch(swipeAll());
+  }
+};
+
+export const goToPrevCard = (): ThunkAction => async (dispatch, getState) => {
+  const state = getState();
+  const deck = Selector.getCurrentDeck(state);
+  const currentIndex = deck.currentIndex - 1;
+  if (currentIndex >= 0) {
+    await dispatch(type.deckBulkInsert([{ ...deck, currentIndex }]));
+  } else {
+    // await dispatch(goBack());
+  }
+};
+
+export const goToCard = (card: Card): ThunkAction => async (
+  dispatch,
+  getState
+) => {
+  const state = getState();
+  const cards = Selector.getCurrentCardList(getState());
+  const deck = state.deck.byId[card.deckId];
+  let currentIndex = 0;
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].id == card.id) {
+      currentIndex = i;
+    }
+  }
+  dispatch(type.deckBulkInsert([{ ...deck, currentIndex }]));
+};
+
+export const goToCardByIndex = (
+  deck: Deck,
+  currentIndex: number
+): ThunkAction => async (dispatch, getState) => {
+  dispatch(type.deckBulkInsert([{ ...deck, currentIndex }]));
+};
+
+export const swipeAll = () => async (dispatch, getState) => {
+  const deck = Selector.getCurrentDeck(getState());
+  await dispatch(type.deckBulkInsert([{ ...deck, currentIndex: 0 }]));
+  // await dispatch(goBack());
+};
+
+const swipeMapping = {
+  // goBack,
+  goToPrevCard,
+  goToNextCard,
+  goToNextCardMastered,
+  goToNextCardNotMastered,
+  goToNextCardToggleMastered,
+};
+
+const cardSwipe = (direction): ThunkAction => async (dispatch, getState) => {
+  const config = getState().config;
+  if (config.hideBodyWhenCardChanged) {
+    await dispatch(updateConfig({ showBody: false, showHint: false }));
+  }
+  const f = swipeMapping[config[direction]];
+  if (f) {
+    await dispatch(f());
+  } else {
+    console.log(`${direction} action is not found`);
+  }
+};
+export const cardSwipeUp = () => cardSwipe('cardSwipeUp');
+export const cardSwipeDown = () => cardSwipe('cardSwipeDown');
+export const cardSwipeLeft = () => cardSwipe('cardSwipeLeft');
+export const cardSwipeRight = () => cardSwipe('cardSwipeRight');
