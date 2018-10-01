228f9ab043bebb032c8e6774576a5bd1361d299a
her0e1c1 <her0e1c1@gmail.com>
2018-10-01 13:22:31 +0700

    rm withRouter

--- src/web/component/deck.tsx
+++ src/web/component/deck.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Table, Icon, AutoComplete, Popconfirm } from 'antd';
 import { ColumnProps } from 'antd/lib/table/interface';
-import { withRouter } from 'react-router-dom';
 import * as Action from 'src/web/action';
 import { Link } from 'react-router-dom';
 
@@ -83,4 +82,4 @@ class _DeckList extends React.Component<ConnectedProps> {
   }
 }
 
-export const DeckList = withRouter(connect(state => ({ state }))(_DeckList));
+export const DeckList = connect(state => ({ state }))(_DeckList);
