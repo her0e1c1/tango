92fb1e23f5007237fbac978303a796b8af43115d
her0e1c1 <her0e1c1@gmail.com>
2018-10-02 23:45:06 +0700

    ignore ts error

--- src/web/index.view.tsx
+++ src/web/index.view.tsx
@@ -20,18 +20,21 @@ class Root extends React.Component {
     // postMessage("string", location.origin)
 
     // on react native, you need to use document instead
-    document.addEventListener('message', message => {
-      __DEV__ && console.log('DEBUG MESSAGE: ', message);
-      // webpack also sends message which is not string but object
-      const data = message.data;
-      if (typeof data === 'string') this.setState({ data });
-    }, false);
+    document.addEventListener(
+      'message',
+      message => {
+        __DEV__ && console.log('DEBUG MESSAGE: ', message);
+        // @ts-ignore
+        const data = message.data;
+        // webpack also sends message which is not string but object
+        if (typeof data === 'string') this.setState({ data });
+      },
+      false
+    );
   }
   render() {
     return (
-      <div>test
       <div dangerouslySetInnerHTML={{ __html: convert(this.state.data) }} />
-      </div>
     );
   }
 }
