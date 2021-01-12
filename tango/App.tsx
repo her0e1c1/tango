1376cc3be79cc607d17fdfaf6a15e17d8f000cd9
her0e1c1 <her0e1c1@gmail.com>
2021-01-12 22:28:28 +0900

    Added header

--- tango/App.tsx
+++ tango/App.tsx
@@ -1,13 +1,26 @@
+/// <reference path="../../../" />
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as NB from 'native-base';
+
+export const Header = (props: {
+  left?: { onPress?: Callback };
+  body?: { title: string; onPress?: Callback };
+  right?: { icon: string; onPress?: Callback };
+}) => (
+  <NB.Header></NB.Header>
+);
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NB.Root>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app.</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NB.Root>
   );
 }
 
