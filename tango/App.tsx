5e76691fa85cd2b027ffabf45ee4571ed6f5d9be
her0e1c1 <her0e1c1@gmail.com>
2021-01-13 23:55:33 +0900

    show Config.tsx

--- tango/App.tsx
+++ tango/App.tsx
@@ -1,11 +1,15 @@
 /// <reference path="../../../" />
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import * as RN from 'react-native';
 import * as NB from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import store from './src/react-native/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+import { persistStore } from 'redux-persist';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -19,41 +23,55 @@ const Stack = createStackNavigator();
 export const HomeScreen = () => (
   <View style={styles.container}>
     <Text>Open up App.tsx to start working on your app</Text>
-    <StatusBar style="auto" />
   </View>
 );
 
+class ErrorBoundary extends React.Component {
+  componentDidCatch(error) {
+    // alert('ERROR: ' + error.toString());
+  }
+  render() {
+    return this.props.children;
+  }
+}
+
 export default function App() {
   return (
-    <NB.Root>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              if (route.name === 'Home') {
-                iconName = focused
-                  ? 'ios-information-circle'
-                  : 'ios-information-circle-outline';
-              } else if (route.name === 'Settings') {
-                iconName = focused ? 'ios-list-box' : 'ios-list';
-              }
-              return <Icon name={iconName} size={size} color={color} />;
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-          }}
-        >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Config" component={ConfigPage} />
-          {/* <Stack.Navigator>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <PersistGate loading={<View />} persistor={persistStore(store)}>
+          <NB.Root>
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                      iconName = focused
+                        ? 'ios-information-circle'
+                        : 'ios-information-circle-outline';
+                    } else if (route.name === 'Settings') {
+                      iconName = focused ? 'ios-list-box' : 'ios-list';
+                    }
+                    return <Icon name={iconName} size={size} color={color} />;
+                  },
+                })}
+                tabBarOptions={{
+                  activeTintColor: 'tomato',
+                  inactiveTintColor: 'gray',
+                }}
+              >
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Config" component={ConfigPage} />
+                {/* <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator> */}
-        </Tab.Navigator>
-      </NavigationContainer>
-    </NB.Root>
+              </Tab.Navigator>
+            </NavigationContainer>
+          </NB.Root>
+        </PersistGate>
+      </Provider>
+    </ErrorBoundary>
   );
 }
 
