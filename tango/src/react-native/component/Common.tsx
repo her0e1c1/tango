9b51efcd783e087aa7c332fbb71503f7ed7fca0a
her0e1c1 <her0e1c1@gmail.com>
2021-01-26 21:49:26 +0900

    removed

--- tango/src/react-native/component/Common.tsx
+++ tango/src/react-native/component/Common.tsx
@@ -1,28 +1,5 @@
 import * as React from "react";
-import * as NB from "native-base";
 import * as RN from "react-native";
-import styled from "styled-components/native";
-
-export const CenteredView = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #f5fcff;
-`;
-
-const Content = styled(NB.Content)`
-  flex: 1;
-  margin-top: 20;
-  background-color: #f5fcff;
-`;
-
-export const Container = (props) => (
-  <NB.Container style={{ backgroundColor: "#f5fcff" }}>
-    <RN.ScrollView>
-      <Content {...props} />
-    </RN.ScrollView>
-  </NB.Container>
-);
 
 export const TouchableOpacity = (props: {
   onPress?: () => void;
@@ -33,8 +10,8 @@ export const TouchableOpacity = (props: {
       {props.children}
     </RN.TouchableOpacity>
   ) : (
-    props.children
-  );
+      props.children
+    );
 };
 export const LoadingIcon = React.memo(
   (props: { isLoadingNoAction?: boolean }) => {
@@ -61,13 +38,6 @@ export const LoadingIcon = React.memo(
       </RN.View>
     );
   }
-);
-
-export const Center = (props) => (
-  <NB.View
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    {...props}
-  />
 );
 
 export const Overlay = (props: {
