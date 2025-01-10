3b83f536c45fe2cf0d8f5d81bb50f640116074ad
her0e1c1 <her0e1c1@gmail.com>
2025-01-10 12:50:15 +0900

    Add samples for binarysearch (#88)

--- sample/test/binarysearch/test_bisect_left.py
+++ sample/test/binarysearch/test_bisect_left.py
@@ -11,20 +11,33 @@ from bisect import bisect_left
 where an element greater than or equal to `a` exists
 """
 
+
+def my_bisect_left(sl, a):
+    lo, hi = 0, len(sl)
+    while lo < hi:
+        mi = (lo + hi) // 2
+        if sl[mi] >= a:
+            hi = mi
+        else:
+            lo = mi + 1
+    return hi
+
+
 @pytest.mark.parametrize(
     "args,expected",
     [
-                # 0  1  2  3  4  5  6  7  8  9
+        # 0  1  2  3  4  5  6  7  8  9
         [[(sl := [1, 1, 1, 3, 4, 4, 4, 5, 6, 6]), 0], 0],
         [[sl, 1], 0],
         [[sl, 2], 3],
         [[sl, 3], 3],
-        [[sl, 4], 3],
+        [[sl, 4], 4],
         [[sl, 5], 7],
         [[sl, 6], 8],
         [[sl, 7], 10],  # = len(sl) if out of index
         [[sl, 12345], 10],
     ],
 )
-def test_bisect_left(args, expected):
+def test(args, expected):
     assert bisect_left(*args) == expected
+    assert my_bisect_left(*args) == expected
