2c908773d5b3d969fbfaf49c1706ec052a88a644
her0e1c1 <her0e1c1@gmail.com>
2025-02-11 19:33:37 +0900

    Added a card for LIS (#95)
    Co-authored-by: studio2022 <studio2022@studio.local>

--- sample/test/binarysearch/test_bisect_left.py
+++ sample/test/binarysearch/test_bisect_left.py
@@ -26,7 +26,6 @@ def my_bisect_left(sl, a):
 @pytest.mark.parametrize(
     "args,expected",
     [
-        # 0  1  2  3  4  5  6  7  8  9
         [[(sl := [1, 1, 1, 3, 4, 4, 4, 5, 6, 6]), 0], 0],
         [[sl, 1], 0],
         [[sl, 2], 3],
