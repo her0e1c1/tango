863b27a4fb8ef662bf63eff6314b1e77c47d5a73
studio2022 <studio2022@studio.local>
2025-02-11 19:28:43 +0900

    Added a card for LIS

---
+++ sample/test/binarysearch/test_lis.py
@@ -0,0 +1,31 @@
+"""
+Given an array of numbers, return the length of the longest increasing subsequence (LIS)
+in which the elements are in strictly increasing order.
+"""
+
+### __FRONT_TEXT_END__
+import pytest
+from bisect import bisect_left
+
+
+def lis(nums):
+    dp = []
+    for n in nums:
+        i = bisect_left(dp, n)
+        if i == len(dp):
+            dp.append(n)
+        else:
+            dp[i] = n
+    return len(dp)
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+      [[1], 1],
+      [[3, 2, 5, 1, 3, 1, 7, 4], 3],  # [3, 5, 7]
+      [[4, 2, 3, 1, 5], 3],  # [2, 3, 6]
+    ],
+)
+def test(args, expected):
+    assert lis(args) == expected
