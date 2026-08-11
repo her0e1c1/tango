a7129d0f3e2c6ac3edffed8f714dbfed41dd842e
her0e1c1 <her0e1c1@gmail.com>
2026-08-11 20:10:43 +0900

    export deck category model

--- src/entities/deck/index.ts
+++ src/entities/deck/index.ts
@@ -1,1 +1,3 @@
-export type { Category, Deck, DeckEdit, DeckId, DeckNew, DeckRaw } from "./model/deck";
+export { CATEGORY, LANGUAGES, getCategory } from "./model/category";
+export type { Category } from "./model/category";
+export type { Deck, DeckEdit, DeckId, DeckNew, DeckRaw } from "./model/deck";
