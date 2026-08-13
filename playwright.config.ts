1435a9581ab84188785061d9969f2da444a0d59d
her0e1c1 <her0e1c1@gmail.com>
2026-08-13 10:43:04 +0900

    test: reorganize test directories into test/e2e and test/integration (#659)
    * test: reorganize test directories into test/e2e and test/integration
    
    * test: rename AuthLogout.integration.spec.tsx to AuthLogout.spec.tsx
    
    ---------
    
    Co-authored-by: studio2022 <studio2022@studio.local>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./e2e",
-  testMatch: "**/*.e2e.ts",
+  testDir: "./test/e2e",
+  testMatch: "**/*.spec.ts",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
