b7905c20f7bab678bef1b1a84fe6c0a58581bd52
her0e1c1 <her0e1c1@gmail.com>
2026-08-14 15:25:26 +0900

    Remove AuthBootstrap wrapper

--- src/app/providers/auth/AuthBootstrap.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, type PropsWithChildren } from "react";
-
-import { startAuthSession } from "./authLifecycle";
-
-export const AuthBootstrap = ({ children }: PropsWithChildren) => {
-  useEffect(startAuthSession, []);
-
-  return children;
-};
