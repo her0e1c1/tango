aec75ade49467d2149d0f715e6668dd756413d2d
studio2022 <studio2022@studio.local>
2026-08-14 13:24:45 +0900

    Simplify strict Knip execution

--- knip.ts
+++ knip.ts
@@ -1,19 +1,14 @@
 import type { KnipConfig } from "knip";
 
-const config: KnipConfig = ({ production }) =>
-  production
-    ? {
-        entry: ["src/app/main.tsx!"],
-        project: ["src/**/*.{ts,tsx}!", "!src/**/*.{spec,test}.{ts,tsx}!", "!src/**/*.stories.{ts,tsx}!"],
-        includeEntryExports: true,
-        storybook: false,
-        treatConfigHintsAsErrors: true,
-        vitest: false,
-      }
-    : {
-        entry: ["public/storybookServiceWorker.js", "steiger.config.ts"],
-        includeEntryExports: true,
-        treatConfigHintsAsErrors: true,
-      };
+const config: KnipConfig = {
+  entry: ["src/app/main.tsx!"],
+  project: [
+    "src/**/*.{ts,tsx}!",
+    "!src/**/*.{spec,test}.{ts,tsx}!",
+    "!src/**/*.stories.{ts,tsx}!",
+  ],
+  includeEntryExports: true,
+  treatConfigHintsAsErrors: true,
+};
 
 export default config;
