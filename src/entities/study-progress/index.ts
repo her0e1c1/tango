61829053b39a9c4f3ebb4a9d6b9e668d7aeb6f5e
studio2022 <studio2022@studio.local>
2026-08-15 19:47:57 +0900

    fix: remove unused study rating export

--- src/entities/study-progress/index.ts
+++ src/entities/study-progress/index.ts
@@ -6,4 +6,4 @@ export {
   recordStudyProgress,
   resolveStudyRating,
 } from "./model/rules";
-export type { StudyProgress, StudyProgressEdit, StudyRating } from "./model/types";
+export type { StudyProgress, StudyProgressEdit } from "./model/types";
