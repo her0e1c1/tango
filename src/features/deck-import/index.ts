d2ae14f024f9cd45b5539c027f6c5ab64966f19b
studio2022 <studio2022@studio.local>
2026-08-16 11:53:53 +0900

    refactor: make sample Deck setup a hook directly

--- src/features/deck-import/index.ts
+++ src/features/deck-import/index.ts
@@ -1,4 +1,4 @@
 export { useDeckImport } from "./hooks/useDeckImport";
-export { useAddSampleDeck } from "./hooks/useAddSampleDeck";
+export { useAddSampleDeck } from "./model/sampleDeck";
 export { downloadSampleCsv, SAMPLE_CSV_TEXT } from "./sampleCsv";
 export { DeckImportView } from "./ui/DeckImportView";
