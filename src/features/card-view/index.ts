51ca32c55a1f48a4073bb32b386be9d89c6e9e82
studio2022 <studio2022@studio.local>
2026-08-30 10:41:32 +0900

    refactor: move card view model to page

--- src/features/card-view/index.ts
+++ src/features/card-view/index.ts
@@ -1,4 +1,3 @@
-export { useCardViewContent } from "./model/useCardViewContent";
 export { BackText } from "./ui/BackText";
 export { CardOverlay } from "./ui/CardOverlay";
 export { CardView } from "./ui/CardView";
