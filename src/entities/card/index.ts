b32d3335bf13f182b1ac1cbd4b5e5796fe422cdb
her0e1c1 <2764757+her0e1c1@users.noreply.github.com>
2026-09-24 14:47:40 +0900

    Export batched Card creation

--- src/entities/card/index.ts
+++ src/entities/card/index.ts
@@ -30,5 +30,5 @@ export { FrontText } from "./ui/FrontText";
 
 export { calculateFsrsState, classifyFsrsState, getStudyRetrievability, studyRetentionTarget } from "./model/fsrsRules";
 export { fsrsStateSchema, type FsrsState } from "./model/fsrs";
-export { writeCardFsrs } from "./api/firestore";
+export { writeCardCreate, writeCardFsrs } from "./api/firestore";
 export { readCardsForTagUpdate, writeCardTagChanges } from "./api/firestore";
